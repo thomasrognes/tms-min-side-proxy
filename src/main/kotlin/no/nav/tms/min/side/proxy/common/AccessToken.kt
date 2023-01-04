d958d3cdd34e403c38cf48349f6a4d368e356a91
rannveigskjerve <55136985+rannveigskjerve@users.noreply.github.com>
2023-01-04 12:14:11 +0100

    Ktor2 (#7)
    * refaktorer startup
    
    * bruk String istedet for AccessToken
    
    * oppdater dependencies versjon
    
    * oppgradere ktor-versjon
    
    * rydd opp i helsesjekk kode
    
    * koderydding
    
    * sett opp testapplikajson
    
    * nytt auth-oppsett
    
    * mindre token-håndtering
    
    * test med autentisering på plass
    
    * legg til ktor status pages og parametriser test for arbeid, dittnav og sykefravært
    
    * kall til rot i destinasjonsapplikasjon
    
    * støtt nested proxy paths
    
    * slå sammen consumere
    
    * slå sammen api
    
    * bruk idporten-sidecar biblioteket
    
    * fiks loggoppsett
    
    * legg til context-path
    
    * bruk envconfig i appoppsett
    
    * fiks port
    
    * fiks port .. igjen
    
    * fiks skrivefeil i rootpath
    
    * debug config
    
    * debug
    
    * debug
    
    * mer debug logging
    
    * legg på port i config
    
    * fiks utkast-env
    
    * slå sammen filer og navnendringer
    
    * slå sammen config og oppstart

--- src/main/kotlin/no/nav/tms/min/side/proxy/common/AccessToken.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.tms.min.side.proxy.common
-
-data class AccessToken(val value: String) {
-    override fun toString(): String {
-        return "***"
-    }
-}
---
+++ src/main/kotlin/no/nav/tms/min/side/proxy/proxyApi.kt
@@ -0,0 +1,72 @@
+package no.nav.tms.min.side.proxy
+
+
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStopping
+import io.ktor.server.application.install
+import io.ktor.server.auth.authenticate
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.plugins.cors.routing.CORS
+import io.ktor.server.plugins.defaultheaders.DefaultHeaders
+import io.ktor.server.plugins.statuspages.StatusPages
+import io.ktor.server.response.respond
+import io.ktor.server.routing.routing
+import mu.KotlinLogging
+import no.nav.tms.token.support.idporten.sidecar.LoginLevel.LEVEL_3
+import no.nav.tms.token.support.idporten.sidecar.installIdPortenAuth
+
+private val log = KotlinLogging.logger {}
+fun Application.proxyApi(
+    corsAllowedOrigins: String,
+    corsAllowedSchemes: String,
+    contentFetcher: ContentFetcher,
+    idportenAuthInstaller: Application.() -> Unit = {
+        installIdPortenAuth {
+            setAsDefault = true
+            loginLevel = LEVEL_3
+        }
+    }
+) {
+
+    install(DefaultHeaders)
+
+    install(StatusPages) {
+        exception<Throwable> { call, cause ->
+            log.info {
+                "Ukjent feil i proxy ${cause.message}"
+            }
+            call.respond(HttpStatusCode.InternalServerError)
+        }
+    }
+
+    idportenAuthInstaller()
+    install(CORS) {
+        allowHost(host = corsAllowedOrigins, schemes = listOf(corsAllowedSchemes))
+        allowCredentials = true
+        allowHeader(HttpHeaders.ContentType)
+        allowMethod(HttpMethod.Options)
+    }
+
+    install(ContentNegotiation) {
+        json(jsonConfig())
+    }
+
+    routing {
+        healthRoutes()
+        authenticate {
+            proxyRoutes(contentFetcher)
+        }
+    }
+
+    configureShutdownHook(contentFetcher)
+}
+
+private fun Application.configureShutdownHook(contentFetcher: ContentFetcher) {
+    environment.monitor.subscribe(ApplicationStopping) {
+        contentFetcher.shutDown()
+    }
+}
