a38c92bdf95617dae317a7efc811b6df55c53cef
Dependency Admin <personbruker@nav.no>
2023-08-08 08:05:19 +0000

    Bumper rapids and rivers.

--- buildSrc/src/main/kotlin/groups.kt
+++ buildSrc/src/main/kotlin/groups.kt
@@ -2,18 +2,9 @@ import default.*
 
 // Managed by tms-dependency-admin. Overrides and additions should be placed in separate file
 
-object Auth0: Auth0Defaults
-object Avro: AvroDefaults
-object Awaitility: AwaitilityDefaults
-object Bouncycastle: BouncycastleDefaults
-object Brukernotifikasjon: BrukernotifikasjonDefaults
-object Doknotifikasjon: DoknotifikasjonDefaults
 object DittNAVCommonLib: DittNAVCommonLibDefaults
 object Flyway: FlywayDefaults
-object GraphQL: GraphQLDefaults
-object H2Database: H2DatabaseDefaults
 object Hikari: HikariDefaults
-object Influxdb: InfluxdbDefaults
 object JacksonDatatype: JacksonDatatypeDefaults
 object Junit: JunitDefaults
 object Jjwt: JjwtDefaults
@@ -24,22 +15,19 @@ object Kotlin: KotlinDefaults
 object KotlinLogging: KotlinLoggingDefaults
 object Kotlinx: KotlinxDefaults
 object KotliQuery: KotliQueryDefaults
-object Ktor2 {
-    object Server: Ktor2Defaults.ServerDefaults
-    object Client: Ktor2Defaults.ClientDefaults
-    object Serialization: Ktor2Defaults.SerializationDefaults
-    object Test: Ktor2Defaults.TestDefaults
+object Ktor {
+    object Server: KtorDefaults.ServerDefaults
+    object Client: KtorDefaults.ClientDefaults
+    object Serialization: KtorDefaults.SerializationDefaults
+    object Test: KtorDefaults.TestDefaults
 }
-object Logback: LogbackDefaults
 object Logstash: LogstashDefaults
 object Micrometer: MicrometerDefaults
 object Mockk: MockkDefaults
-object NAV: NAVDefaults
 object Postgresql: PostgresqlDefaults
 object Prometheus: PrometheusDefaults
 object RapidsAndRivers: RapidsAndRiversDefaults
 object Shadow: ShadowDefaults
-object SulkyUlid: SulkyUlidDefaults
 object TestContainers: TestContainersDefaults
+object TmsCommonLib: TmsCommonLibDefaults
 object TmsKtorTokenSupport: TmsKtorTokenSupportDefaults
-object Unleash: UnleashDefaults
