25d5f5bf77ba5be8fde6e9c435fc81f2e485a624
Dependency Admin <personbruker@nav.no>
2024-02-13 11:19:16 +0000

    Bumper tms-common-lib til 3.1.1.

--- buildSrc/src/main/kotlin/default/dependencies.kt
+++ buildSrc/src/main/kotlin/default/dependencies.kt
@@ -216,7 +216,7 @@ interface TestContainersDefaults: DependencyGroup {
 
 interface TmsCommonLibDefaults: DependencyGroup {
     override val groupId get() = "no.nav.tms.common"
-    override val version get() = "3.1.0-v2"
+    override val version get() = "3.1.1"
 
     val metrics get() = dependency("metrics")
     val observability get() = dependency("observability")
